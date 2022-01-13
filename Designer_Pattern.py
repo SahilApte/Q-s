09f2c9cdd80e742f4efad4cb8af3f5bee70155ce
Sahil Apte <71555285+ZIGXAG@users.noreply.github.com>
2022-01-13 10:22:59 +0530

    Update Designer_Pattern.py

--- Designer_Pattern.py
+++ Designer_Pattern.py
@@ -10,10 +10,11 @@ for j in range(n//2-1,-1,-1):
     print((p*((j*2)+1)).center(m,'*'))
 # get differnt patterns by chaging the characters
 """
-Sample Input
+Sample Input:
 9 27
-Sample Output
-9 27
+
+Sample Output:
+
 ************<|>************
 *********<|><|><|>*********
 ******<|><|><|><|><|>******
@@ -23,4 +24,5 @@ Sample Output
 ******<|><|><|><|><|>******
 *********<|><|><|>*********
 ************<|>************
+
 """

5bddd4c909166eac4cfbaba03d0c5731efd08cce
Sahil Apte <71555285+ZIGXAG@users.noreply.github.com>
2022-01-13 10:22:11 +0530

    Update Designer_Pattern.py

--- Designer_Pattern.py
+++ Designer_Pattern.py
@@ -8,4 +8,19 @@ print("I ♡ to Code".center(m,'*'))
 
 for j in range(n//2-1,-1,-1):
     print((p*((j*2)+1)).center(m,'*'))
-# get differnt patterns by chaging the characters 
+# get differnt patterns by chaging the characters
+"""
+Sample Input
+9 27
+Sample Output
+9 27
+************<|>************
+*********<|><|><|>*********
+******<|><|><|><|><|>******
+***<|><|><|><|><|><|><|>***
+********I ♡ to Code********
+***<|><|><|><|><|><|><|>***
+******<|><|><|><|><|>******
+*********<|><|><|>*********
+************<|>************
+"""

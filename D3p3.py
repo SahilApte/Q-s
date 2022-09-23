501cddfa03450b4282c27f0d3c23dd072d340484
Sahil Apte <71555285+SahilApte@users.noreply.github.com>
2022-09-23 22:42:53 +0530

    Update D3p3.py

--- D3p3.py
+++ D3p3.py
@@ -1,3 +1,5 @@
+
+#Approach 1
 import math
 
 def happy(num):
@@ -16,3 +18,15 @@ while(result!=1 and result!=4):
     result =happy(result)
     
 print("true" if result ==1 else "false")
+
+#Approach 2
+def happy(num):
+    lst = []
+    while(num!=1):
+        num = sum(int(i)**2 for i in str(num))
+        if num in lst:
+            return "false"
+        lst.append(num)
+    return "true"
+x = input()
+print(happy(x))

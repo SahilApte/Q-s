83361a992047fdc0e18db4e57fd74d1842a2e469
Sahil Apte <71555285+SahilApte@users.noreply.github.com>
2022-11-26 17:08:43 +0530

    Leet code removing duplicate chars
    /*1047. Remove All Adjacent Duplicates In String
    https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/description/
    Easy
    5.2K
    202
    Companies
    You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
    
    We repeatedly make duplicate removals on s until we no longer can.
    
    Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
    
     
    
    Example 1:
    
    Input: s = "abbaca"
    Output: "ca"
    Explanation: 
    For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
    Example 2:
    
    Input: s = "azxxzy"
    Output: "ay"
     
    
    Constraints:
    
    1 <= s.length <= 105
    s consists of lowercase English letters.*/

---
+++ Dup.java
@@ -0,0 +1,53 @@
+/*1047. Remove All Adjacent Duplicates In String
+https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/description/
+Easy
+5.2K
+202
+Companies
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.*/
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack <Character> st= new Stack<>();
+      for(int i=0;i<s.length();i++)
+      {
+          if(!st.empty() && s.charAt(i)==st.peek())
+          {
+              st.pop();
+          }
+          else
+          {
+              st.push(s.charAt(i));
+          }
+      }
+StringBuilder sb = new StringBuilder();
+    while(!st.empty())
+    {
+        sb.append(st.pop());
+    }
+
+    return sb.reverse().toString();
+    }
+}

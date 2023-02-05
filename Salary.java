540a6688be8034440b88df7f6907b1cbc147f53b
Sahil Apte <71555285+SahilApte@users.noreply.github.com>
2023-02-05 22:04:31 +0530

    Create Salary.java
    1491. Average Salary Excluding the Minimum and Maximum Salary
    Easy
    1.1K
    128
    Companies
    You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
    
    Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
    
     
    
    Example 1:
    
    Input: salary = [4000,3000,1000,2000]
    Output: 2500.00000
    Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
    Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500
    Example 2:
    
    Input: salary = [1000,2000,3000]
    Output: 2000.00000
    Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
    Average salary excluding minimum and maximum salary is (2000) / 1 = 2000
     
    
    Constraints:
    
    3 <= salary.length <= 100
    1000 <= salary[i] <= 106
    All the integers of salary are unique.
    Accepted
    184.7K
    Submissions
    298.5K
    Acceptance Rate
    61.9%

---
+++ Salary.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/description/?envType=study-plan&id=programming-skills-i
+class Salary {
+    public double average(int[] salary) {
+        double sum= 0;
+        Arrays.sort(salary);
+        // System.out.println(salary[0]);
+        for(int i=1;i<salary.length-1;i++)
+        {
+        sum+= salary[i];
+        }
+        return (sum/(salary.length-2));
+        
+    }
+}

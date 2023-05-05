e8f831977113041af11f47dd21b6330769c90cd4
Sahil Apte <71555285+SahilApte@users.noreply.github.com>
2023-05-05 22:04:19 +0530

    TreeIdenticla.java
    Given the roots of two binary trees p and q, write a function to check if they
    are the same or not.
    
    Two binary trees are considered the same if they are structurally identical, 
    and the nodes have the same value.
    
    Sample Testcase:
    -------------------------------------
    input=[1,2,3]
    [1,2,3]
    output=true
    
    Sample Testcase:
    -------------------------------------
    input=[1,2]
    [1,-1,2]
    output=false
    
    Sample Testcase:
    -------------------------------------
    Input=[1,2,1]
    [1,1,2]
    output=false
    
    
    
    /**
     * Definition for a binary tree node.
     * public class TreeNode {
     *     int val;
     *     TreeNode left;
     *     TreeNode right;
     *     TreeNode() {}
     *     TreeNode(int val) { this.val = val; }
     *     TreeNode(int val, TreeNode left, TreeNode right) {
     *         this.val = val;
     *         this.left = left;
     *         this.right = right;
     *     }
     * }
     */
    class Solution {
        public boolean isSameTree(TreeNode p, TreeNode q) {
            
        }
    }

---
+++ TreeIdenticla.java
@@ -0,0 +1,66 @@
+Given the roots of two binary trees p and q, write a function to check if they
+are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, 
+and the nodes have the same value.
+
+Sample Testcase:
+-------------------------------------
+input=[1,2,3]
+[1,2,3]
+output=true
+
+Sample Testcase:
+-------------------------------------
+input=[1,2]
+[1,-1,2]
+output=false
+
+Sample Testcase:
+-------------------------------------
+Input=[1,2,1]
+[1,1,2]
+output=false
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+    }
+}
+
+import java.util.*;
+class Solution {
+    public boolean isSameTree(Node p, Node q) {
+        if(p==null &&q==null)
+        {
+            return true;
+        }
+        if(p==null||q==null)
+        {
+            return false;
+        }
+        if(p.data!=q.data)
+        {
+            return false;
+        }
+            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        
+    }
+}

03ed34f4865c18e035a01c7253ab5fb3e08c005c
Sahil Apte <71555285+SahilApte@users.noreply.github.com>
2023-05-13 22:33:33 +0530

    Sliding_window_avg.java
    /*
    Mary is having a list of values. She wants to find a sub-list having k elements 
    from this, which will have highest average sum. Your task is to help Mary in her 
    task.
        
    Sample Input-1:
    ---------------
    6
    4 14 -8 -2 27 12
    4
    
    Explanation:
    ------------
    Sublist is [14 -8 -2 27]
        
    Sample Output-1:
    ----------------
    7.75
      
    Sample test case
    
    case=1
    input=6
    4 14 -8 -2 27 12
    4
    output=7.75
    */

--- fs_A1_D63p3.java
+++ fs_A1_D63p3.java
@@ -58,3 +58,31 @@ class d63p3{
     }
 
 }
+import java.util.*;
+class Main{
+public static double subListSum(int[] arr, int subSize) {
+    double maxAvg = Double.NEGATIVE_INFINITY;
+    double currSum = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+        currSum += arr[i];
+
+        if (i >= subSize) {
+            currSum -= arr[i - subSize];
+            maxAvg = Math.max(maxAvg, currSum / subSize);
+        }
+    }
+
+    return maxAvg;
+}
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
+        int subSize = in.nextInt();
+        System.out.println(subListSum(arr,subSize));
+    }
+}

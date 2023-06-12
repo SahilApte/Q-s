7836bf17f341f527667078a43535228ebd404fb9
Sahil Apte <71555285+SahilApte@users.noreply.github.com>
2023-06-12 18:07:31 +0530

    ChildSum.java
    https://leetcode.com/problems/root-equals-sum-of-children/description/

---
+++ ChildSum.java
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/root-equals-sum-of-children/description/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        if(root.left.val+root.right.val==root.val)
+        {
+            return true;
+        }
+        return false;
+    }
+}

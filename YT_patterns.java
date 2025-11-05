9f765c88bea3006abfa2d088c0a00904df430448
Sahil Apte <71555285+SahilApte@users.noreply.github.com>
2025-11-05 16:49:09 +0000

    added other way for pascal triangle

--- YT_patterns.java
+++ YT_patterns.java
@@ -138,16 +138,39 @@ public class YT_patterns {
     }
 
     public static void pascalsTriangle(int n){
-        for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
                 System.out.print(" ");
             }
             int coef=1;
-            for(int j=1;j<=n-i+1;j++){
+            // for(int j=1;j<=n-i+1;j++){
+            //     System.out.print(coef+" ");
+            //     coef=coef*(n-i+1-j)/j;
+            // }
+            for(int j=0;j<=i;++j){
                 System.out.print(coef+" ");
-                coef=coef*(n-i+1-j)/j;
+                coef = coef*(i-j)/(j+1);
             }
             
+            System.out.println();
+        }
+    }
+
+//     1
+//    1 2
+//   1 2 3
+//  1 2 3 4
+// 1 2 3 4 5
+
+
+    public static void halfPyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
             System.out.println();
         }
     }
@@ -166,5 +189,6 @@ public class YT_patterns {
         // hollowButterFly(10);
         // hollowRhombus(5);
         pascalsTriangle(5);
+        // halfPyramid(5);
     }
 }
